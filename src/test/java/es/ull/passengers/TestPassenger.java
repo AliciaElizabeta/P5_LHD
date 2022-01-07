7084395719f901e4040a57dd4c1112d87697fe87
Alicia Marrero Ravelo <alu0101221960@ull.edu.es>
2022-01-07 14:16:30 +0000

    Add Test Passenger

---
+++ src/test/java/es/ull/passengers/TestPassenger.java
@@ -0,0 +1,32 @@
+package es.ull.passengers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestPassenger {
+
+    private Passenger passenger;
+
+    @BeforeEach 
+    void setup() throws Exception {
+        passenger = new Passenger("012", "Bob", "US");
+    }
+
+    @Test
+    void IdentifierTest() {
+        assertEquals(("012"), passenger.getIdentifier());
+    }
+
+    @Test
+    void NameTest() {
+        assertEquals(("Bob"), passenger.getName());
+    }
+
+    @Test
+    void CountryTest() {
+        assertEquals(("US"), passenger.getCountryCode());
+    }
+}
